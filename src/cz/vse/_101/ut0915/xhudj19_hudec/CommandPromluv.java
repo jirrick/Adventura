5694e989be4715442492ee250ffa3c9c637be893
Jirka Hudec <jurahudec@gmail.com>
2013-01-12 17:26:41 +0100

    CommandPromluv

---
+++ src/cz/vse/_101/ut0915/xhudj19_hudec/CommandPromluv.java
@@ -0,0 +1,77 @@
+package cz.vse._101.ut0915.xhudj19_hudec;
+
+/* Soubor je ulozen v kodovani UTF-8.
+ * Kontrola kódování: Příliš žluťoučký kůň úpěl ďábelské ódy.
+ */
+
+import static cz.vse._101.ut0915.xhudj19_hudec.Texts.*;
+
+/**
+ * *****************************************************************************
+ * Instance třídy {@code CommandNastraz} představují ...
+ *
+ * @author jméno autora
+ * @version 0.00.0000 — 20yy-mm-dd
+ */
+public class CommandPromluv extends ACommand
+{
+    //== KONSTANTNÍ ATRIBUTY TŘÍDY =============================================
+    //== PROMĚNNÉ ATRIBUTY TŘÍDY ===============================================
+    //== STATICKÝ INICIALIZAČNÍ BLOK - STATICKÝ KONSTRUKTOR ====================
+    //== KONSTANTNÍ ATRIBUTY INSTANCÍ ==========================================
+    //== PROMĚNNÉ ATRIBUTY INSTANCÍ ============================================
+    //== PŘÍSTUPOVÉ METODY VLASTNOSTÍ TŘÍDY ====================================
+    //== OSTATNÍ NESOUKROMÉ METODY TŘÍDY =======================================
+    //##########################################################################
+    //== KONSTRUKTORY A TOVÁRNÍ METODY =========================================
+
+     /*************************************************************************
+     *Vytvoří příkaz nastražující pastičku.
+     */
+    public CommandPromluv()
+    {
+        super("promluv",
+              "Pokusí se o rozhovor s danou postavou");
+    }
+
+
+    //== ABSTRAKTNÍ METODY =====================================================
+    //== PŘÍSTUPOVÉ METODY VLASTNOSTÍ INSTANCÍ =================================
+    //== OSTATNÍ NESOUKROMÉ METODY INSTANCÍ ====================================
+
+    /*************************************************************************
+     * Instance třídy CommandNastraz představují příkazy realizující
+     * nastražení pastičky. Pokud je v místnosti myš a v rukou pastička,
+     * vyndá z rukou pastičku a vloží do nich myš a to samé i v místnosti.
+     */
+
+    @Override
+    public String execute(String... arguments)
+    {
+        if (arguments.length < 2) {
+            return nROZHOVOR_NIKDO + status();
+        }
+
+
+        return "";
+    }
+
+
+
+//    == SOUKROMÉ A POMOCNÉ METODY TŘÍDY =======================================
+//    == SOUKROMÉ A POMOCNÉ METODY INSTANCÍ ====================================
+//    == INTERNÍ DATOVÉ TYPY ===================================================
+//    == TESTOVACÍ METODY A TŘÍDY ==============================================
+//
+//         /********************************************************************
+//          * Testovací metoda.
+//          */
+//         public static void test()
+//         {
+//             CommandNastraz instance = new CommandNastraz();
+//         }
+//         /** @param args Parametry příkazového řádku - nepoužívané. */
+//         public static void main(String[] args)  {  test();  }
+}
+
+
